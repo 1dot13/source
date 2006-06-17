d0a4c8f7fd7316e1ab1f2b61c0c3528495a16828
MaddMugsy <MaddMugsy@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-06-17 20:28:52 +0000

    -version update
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@253 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- GameVersion.cpp
+++ GameVersion.cpp
@@ -23,12 +23,12 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13.249" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13.253" };
 
 
 #endif
 
-INT8		czVersionNumber[16]	= { "Build 06.06.07" };
+INT8		czVersionNumber[16]	= { "Build 06.06.17" };
 INT16		zTrackingNumber[16]	= { L"Z" };
 	
 
