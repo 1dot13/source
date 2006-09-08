93204e907d58e4bfc09d47414674bbc5beac5837
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-09-08 21:22:00 +0000

    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@518 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- GameVersion.cpp
+++ GameVersion.cpp
@@ -23,12 +23,12 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13.502" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13.518" };
 
 
 #endif
 
-INT8		czVersionNumber[16]	= { "Build 06.09.01" };
+INT8		czVersionNumber[16]	= { "Build 06.09.08" };
 INT16		zTrackingNumber[16]	= { L"Z" };
 	
 
