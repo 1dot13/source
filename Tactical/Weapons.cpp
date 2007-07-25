3e997fa0e1819778cecde169e61e7d19d3c14652
SpaceViking <SpaceViking@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-07-25 12:41:40 +0000

    Don't have monster spit cause fiery explosions
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@1120 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Tactical/Weapons.cpp
+++ Tactical/Weapons.cpp
@@ -2900,7 +2900,10 @@ BOOLEAN DoSpecialEffectAmmoMiss( UINT8 ubAttackerID, INT16 sGridNo, INT16 sXPos,
 
 		PlayJA2Sample( CREATURE_GAS_NOISE, RATE_11025, SoundVolume( HIGHVOLUME, sGridNo ), 1, SoundDir( sGridNo ) );			
 
-      	NewSmokeEffect( sGridNo, usItem, 0, ubAttackerID );
+// WDS fix 07/25/2007
+// Don't have monsters cause explosions
+  //    	NewSmokeEffect( sGridNo, usItem, 0, ubAttackerID );
+
   //  // Do Spread effect.......
   //  switch( usItem )
   //  {
