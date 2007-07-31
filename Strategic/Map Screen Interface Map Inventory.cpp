99358cf1034d5a54e5ea48e30e1a001193ecd0f1
AalaarDB <AalaarDB@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-07-31 22:45:34 +0000

    Uploaded the wrong file
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@1141 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Strategic/Map Screen Interface Map Inventory.cpp
+++ Strategic/Map Screen Interface Map Inventory.cpp
@@ -1463,7 +1463,7 @@ void BeginInventoryPoolPtr( OBJECTTYPE *pInventorySlot )
 			fMapInventoryItem = FALSE;
 			if ( _KeyDown ( 89 )) //Lalien: sell all items of this type on Alt+Y 
 			{
-				for( UINT32 iNumber = 0 ; iNumber <  pInventoryPoolList.size() ; ++iNumber)
+				for( UINT32 iNumber = 0; iNumber < iTotalNumberOfSlots ; ++iNumber)
 				{
 					if ( pInventoryPoolList[ iNumber ].o.usItem == gItemPointer.usItem )
 					{
