5926a62a0564d92a4424de767a3ae30986feee19
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-08-05 00:47:19 +0000

    - allowed militia to wear camo
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@1155 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Tactical/Soldier Create.cpp
+++ Tactical/Soldier Create.cpp
@@ -540,7 +540,7 @@ SOLDIERTYPE* TacticalCreateSoldier( SOLDIERCREATE_STRUCT *pCreateStruct, UINT8 *
 			if( guiCurrentScreen != AUTORESOLVE_SCREEN )
 			{
 				// also, if an army guy has camouflage, roll to determine whether they start camouflaged
-				if ( Soldier.bTeam == ENEMY_TEAM )
+				if ( ( Soldier.bTeam == ENEMY_TEAM ) || ( Soldier.bTeam == MILITIA_TEAM ) )
 				{
 					i = FindCamoKit ( &Soldier );
 
