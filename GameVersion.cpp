29b883f59d3c6137a5e700459f7ed1e52f12db46
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-08-14 18:23:58 +0000

    new exe build (1202)
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@1203 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- GameVersion.cpp
+++ GameVersion.cpp
@@ -13,7 +13,7 @@
 #ifdef JA2BETAVERSION
 
 //BETA/TEST BUILD VERSION
-CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.1080" }; 
+CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.1202" }; 
 
 #elif defined CRIPPLED_VERSION
 
@@ -23,12 +23,12 @@ CHAR16 zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION  
-	CHAR16		zVersionLabel[256]		= { L"Release v1.13.1147" };
+	CHAR16		zVersionLabel[256]		= { L"Release v1.13.1202" };
 
 
 #endif
 										
-CHAR8		czVersionNumber[16]	= { "Build 07.08.02" };		//YY.MM.DD
+CHAR8		czVersionNumber[16]	= { "Build 07.08.14" };		//YY.MM.DD
 CHAR16		zTrackingNumber[16]	= { L"Z" };
 	
 
