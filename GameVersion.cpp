9dfd4e096ecf54e7697af004efd8bf016d52dc9f
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2008-05-12 09:48:10 +0000

    new exe build (2146) which also contains multiplayer
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@2146 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- GameVersion.cpp
+++ GameVersion.cpp
@@ -13,12 +13,12 @@
 #ifdef JA2EDITOR
 
 //MAP EDITOR BUILD VERSION
-CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.2118" }; 
+CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.2146" }; 
 
 #elif defined JA2BETAVERSION
 
 //BETA/TEST BUILD VERSION
-CHAR16 zVersionLabel[256]		= { L"Debug v1.13.2118" }; 
+CHAR16 zVersionLabel[256]		= { L"Debug v1.13.2146" }; 
 
 #elif defined CRIPPLED_VERSION
 
@@ -28,11 +28,11 @@ CHAR16 zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION	
-	CHAR16		zVersionLabel[256]		= { L"Release v1.13.2118" };
+	CHAR16		zVersionLabel[256]		= { L"Release v1.13.2146" };
 
 #endif
 										
-CHAR8		czVersionNumber[16]	= { "Build 08.05.08" };		//YY.MM.DD
+CHAR8		czVersionNumber[16]	= { "Build 08.05.12" };		//YY.MM.DD
 CHAR16		zTrackingNumber[16]	= { L"Z" };
 	
 
