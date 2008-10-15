260b2bc4fd26922fe5ac98de6727225c65a064bb
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2008-10-15 21:00:53 +0000

    New Map Editor and Game Exe Build (Build: 2377)
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@2378 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- GameVersion.cpp
+++ GameVersion.cpp
@@ -13,12 +13,12 @@
 #ifdef JA2EDITOR
 
 //MAP EDITOR BUILD VERSION
-CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.2375" }; 
+CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.2377" }; 
 
 #elif defined JA2BETAVERSION
 
 //BETA/TEST BUILD VERSION
-CHAR16 zVersionLabel[256]		= { L"Debug v1.13.2375" }; 
+CHAR16 zVersionLabel[256]		= { L"Debug v1.13.2377" }; 
 
 #elif defined CRIPPLED_VERSION
 
@@ -32,7 +32,7 @@ CHAR16 zVersionLabel[256]		= { L"Beta v. 0.98" };
 
 #endif
 										
-CHAR8		czVersionNumber[16]	= { "Build 08.10.13" };		//YY.MM.DD
+CHAR8		czVersionNumber[16]	= { "Build 08.10.15" };		//YY.MM.DD
 CHAR16		zTrackingNumber[16]	= { L"Z" };
 	
 
