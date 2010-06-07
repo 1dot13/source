b4a906728b380726bd97206a52d8e4d1e82f864c
inshy <inshy@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2010-06-07 06:04:39 +0000

    Translated new lines for _Ja25ItalianText.cpp
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@3524 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Utils/_Ja25ItalianText.cpp
+++ Utils/_Ja25ItalianText.cpp
@@ -73,16 +73,16 @@ STR16	gzIronManModeWarningText[]=
 
 STR16 gzDisplayCoverText[]=
 {
-	L"Cover: %d/100 %s, Brightness: %d/100",
-	L"Gun Range: %d/%d tiles, Chance to hit: %d/100",
-	L"Disabling cover display",
-	L"Showing mercenary view",
-	L"Showing danger zones for mercenary",
-	L"Wood", // wanted to use jungle , but wood is shorter in german too (dschungel vs wald)
-	L"Urban",
-	L"Desert",
-	L"Snow", // NOT USED!!!
-	L"Wood and Desert",
+	L"Copertura: %d/100 %s, Luminosità: %d/100",
+	L"Gittata dell'arma: %d/%d tiles, Probabilità di colpire: %d/100",
+	L"Disabilita visualizzazione copertura",
+	L"Mostra visuale del mercenario",
+	L"Mostra zone di pericolo per il mercenario",
+	L"Bosco", // wanted to use jungle , but wood is shorter in german too (dschungel vs wald)
+	L"Urbano",
+	L"Deserto",
+	L"Neve", // NOT USED!!!
+	L"Bosco e deserto",
 	L"" // yes empty for now
 };
 
