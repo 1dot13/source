e39c82e0d562ceecd4d7be257373ace3a132d7fd
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2011-01-18 11:38:31 +0000

    ************************************************************************** * Merged Language specific GameDir from Development Trunk: Revision 4063 * ************************************************************************** - Merged from SVN: https://81.169.133.124/source/ja2/branches/Wanne/JA2%201.13%20MP
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameData@4066 3b4a5df2-a311-0410-b5c6-a8a6f20db521

---
+++ gamedir-languages/Dutch_Version/Data/Scripts/Dutch.undergroundsectornames.lua
@@ -0,0 +1,66 @@
+﻿--[[
+
+    This file (or its localized versions respectively) are included in
+    "initunderground.lua" to provide the underground sector names.
+    
+    Make sure to save this file as UTF-8, preferably with BOM signature in
+    order to help editors to not mess up the encoding. Also make sure to not
+    save BOM in any other script unless it explicitly allows that.
+
+]]
+
+sectornames = {
+
+    ["A10-1"] = "Rebel Hideout",
+    
+    ["J9-1"] = "Tixa Dungeon",
+    ["J9-2"] = "Tixa Dungeon",
+    
+    ["K4-1"] = "Orta Basement",
+    
+    ["O3-1"] = "Tunnel",
+    
+    ["P3-1"] = "Shelter",
+    
+    
+    ["B2-1"] = "Chitzena Mine",
+    
+    ["D4-1"] = "San Mona Mine",
+    ["D5-1"] = "San Mona Mine",
+    
+    ["D13-1"] = "Drassen Mine",
+    ["E13-1"] = "Drassen Mine",
+    ["E13-2"] = "Drassen Mine",
+    ["F13-2"] = "Drassen Mine",
+    ["G13-2"] = "Drassen Mine",
+    ["G13-3"] = "Drassen Mine",
+    ["F13-3"] = "Drassen Mine",
+    
+    ["H8-1"] = "Cambria Mine",
+    ["H9-1"] = "Cambria Mine",
+    ["H9-2"] = "Cambria Mine",
+    ["H8-2"] = "Cambria Mine",
+    ["H8-3"] = "Cambria Mine",
+    ["I8-3"] = "Cambria Mine",
+    ["J8-3"] = "Cambria Mine",
+
+    ["I14-1"] = "Alma Mine",
+    ["J14-1"] = "Alma Mine",
+    ["J14-2"] = "Alma Mine",
+    ["J13-2"] = "Alma Mine",
+    ["J13-3"] = "Alma Mine",
+    ["K13-3"] = "Alma Mine",
+    
+    ["H3-1"] = "Grumm Mine",
+    ["I3-1"] = "Grumm Mine",
+    ["I3-2"] = "Grumm Mine",
+    ["H3-2"] = "Grumm Mine",
+    ["H4-2"] = "Grumm Mine",
+    ["H4-3"] = "Grumm Mine",
+    ["G4-3"] = "Grumm Mine",
+    
+    ["P1-1"] = "Demoville",
+    ["P1-2"] = "Demoville",
+ 
+    creatureLair = "Creature Lair",
+}
---
+++ gamedir-languages/Polish_Version/Data-1.13/Scripts/Polish.undergroundsectornames.lua
@@ -0,0 +1,66 @@
+﻿--[[
+
+    This file (or its localized versions respectively) are included in
+    "initunderground.lua" to provide the underground sector names.
+    
+    Make sure to save this file as UTF-8, preferably with BOM signature in
+    order to help editors to not mess up the encoding. Also make sure to not
+    save BOM in any other script unless it explicitly allows that.
+
+]]
+
+sectornames = {
+
+    ["A10-1"] = "Kryjówka rebeliantów",
+    
+    ["J9-1"] = "Tixa Dungeon",
+    ["J9-2"] = "Tixa - Lochy",
+    
+    ["K4-1"] = "Orta - Piwnica",
+    
+    ["O3-1"] = "Tunel",
+    
+    ["P3-1"] = "Schron",
+    
+    
+    ["B2-1"] = "Chitzena Kopalnia",
+    
+    ["D4-1"] = "San Mona Kopalnia",
+    ["D5-1"] = "San Mona Kopalnia",
+    
+    ["D13-1"] = "Drassen Kopalnia",
+    ["E13-1"] = "Drassen Kopalnia",
+    ["E13-2"] = "Drassen Kopalnia",
+    ["F13-2"] = "Drassen Kopalnia",
+    ["G13-2"] = "Drassen Kopalnia",
+    ["G13-3"] = "Drassen Kopalnia",
+    ["F13-3"] = "Drassen Kopalnia",
+    
+    ["H8-1"] = "Cambria Kopalnia",
+    ["H9-1"] = "Cambria Kopalnia",
+    ["H9-2"] = "Cambria Kopalnia",
+    ["H8-2"] = "Cambria Kopalnia",
+    ["H8-3"] = "Cambria Kopalnia",
+    ["I8-3"] = "Cambria Kopalnia",
+    ["J8-3"] = "Cambria Kopalnia",
+
+    ["I14-1"] = "Alma Kopalnia",
+    ["J14-1"] = "Alma Kopalnia",
+    ["J14-2"] = "Alma Kopalnia",
+    ["J13-2"] = "Alma Kopalnia",
+    ["J13-3"] = "Alma Kopalnia",
+    ["K13-3"] = "Alma Kopalnia",
+    
+    ["H3-1"] = "Grumm Kopalnia",
+    ["I3-1"] = "Grumm Kopalnia",
+    ["I3-2"] = "Grumm Kopalnia",
+    ["H3-2"] = "Grumm Kopalnia",
+    ["H4-2"] = "Grumm Kopalnia",
+    ["H4-3"] = "Grumm Kopalnia",
+    ["G4-3"] = "Grumm Kopalnia",
+    
+    ["P1-1"] = "Demoville",
+    ["P1-2"] = "Demoville",
+ 
+    creatureLair = "Gniazdo stworzeń",
+}
