c26fe8ee55ed4ab0a57019a5273e57f36a16889a
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2011-10-28 08:39:54 +0000

    - Updated French GameDir files (by arkantos)
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameData@4766 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- gamedir-languages/French_Version/Data-1.13/Scripts/French.undergroundsectornames.lua
+++ gamedir-languages/French_Version/Data-1.13/Scripts/French.undergroundsectornames.lua
@@ -11,56 +11,56 @@
 
 sectornames = {
 
-    ["A10-1"] = "Rebel Hideout",
+    ["A10-1"] = "Cachette rebelle",
     
-    ["J9-1"] = "Tixa Dungeon",
-    ["J9-2"] = "Tixa Dungeon",
+    ["J9-1"] = "Donjon de Tixa",
+    ["J9-2"] = "Donjon de Tixa",
     
-    ["K4-1"] = "Orta Basement",
+    ["K4-1"] = "Sous-sol d'Orta",
     
     ["O3-1"] = "Tunnel",
     
-    ["P3-1"] = "Shelter",
+    ["P3-1"] = "Abri",
     
     
-    ["B2-1"] = "Chitzena Mine",
+    ["B2-1"] = "Mine de Chitzena",
     
-    ["D4-1"] = "San Mona Mine",
-    ["D5-1"] = "San Mona Mine",
+    ["D4-1"] = "Mine de San Mona",
+    ["D5-1"] = "Mine de San Mona",
     
-    ["D13-1"] = "Drassen Mine",
-    ["E13-1"] = "Drassen Mine",
-    ["E13-2"] = "Drassen Mine",
-    ["F13-2"] = "Drassen Mine",
-    ["G13-2"] = "Drassen Mine",
-    ["G13-3"] = "Drassen Mine",
-    ["F13-3"] = "Drassen Mine",
+    ["D13-1"] = "Mine de Drassen",
+    ["E13-1"] = "Mine de Drassen",
+    ["E13-2"] = "Mine de Drassen",
+    ["F13-2"] = "Mine de Drassen",
+    ["G13-2"] = "Mine de Drassen",
+    ["G13-3"] = "Mine de Drassen",
+    ["F13-3"] = "Mine de Drassen",
     
-    ["H8-1"] = "Cambria Mine",
-    ["H9-1"] = "Cambria Mine",
-    ["H9-2"] = "Cambria Mine",
-    ["H8-2"] = "Cambria Mine",
-    ["H8-3"] = "Cambria Mine",
-    ["I8-3"] = "Cambria Mine",
-    ["J8-3"] = "Cambria Mine",
+    ["H8-1"] = "Mine de Cambria",
+    ["H9-1"] = "Mine de Cambria",
+    ["H9-2"] = "Mine de Cambria",
+    ["H8-2"] = "Mine de Cambria",
+    ["H8-3"] = "Mine de Cambria",
+    ["I8-3"] = "Mine de Cambria",
+    ["J8-3"] = "Mine de Cambria",
 
-    ["I14-1"] = "Alma Mine",
-    ["J14-1"] = "Alma Mine",
-    ["J14-2"] = "Alma Mine",
-    ["J13-2"] = "Alma Mine",
-    ["J13-3"] = "Alma Mine",
-    ["K13-3"] = "Alma Mine",
+    ["I14-1"] = "Mine de Alma",
+    ["J14-1"] = "Mine de Alma",
+    ["J14-2"] = "Mine de Alma",
+    ["J13-2"] = "Mine de Alma",
+    ["J13-3"] = "Mine de Alma",
+    ["K13-3"] = "Mine de Alma",
     
-    ["H3-1"] = "Grumm Mine",
-    ["I3-1"] = "Grumm Mine",
-    ["I3-2"] = "Grumm Mine",
-    ["H3-2"] = "Grumm Mine",
-    ["H4-2"] = "Grumm Mine",
-    ["H4-3"] = "Grumm Mine",
-    ["G4-3"] = "Grumm Mine",
+    ["H3-1"] = "Mine de Grumm",
+    ["I3-1"] = "Mine de Grumm",
+    ["I3-2"] = "Mine de Grumm",
+    ["H3-2"] = "Mine de Grumm",
+    ["H4-2"] = "Mine de Grumm",
+    ["H4-3"] = "Mine de Grumm",
+    ["G4-3"] = "Mine de Grumm",
     
     ["P1-1"] = "Demoville",
     ["P1-2"] = "Demoville",
  
-    creatureLair = "Creature Lair",
+    creatureLair = "Tanière des créatures",
 }
