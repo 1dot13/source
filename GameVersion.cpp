1e1363c6ad4f439a7a5792e890521a85d84a67b1
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2011-12-28 10:51:54 +0000

    2011 Release Version: 4870
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@4870 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- GameVersion.cpp
+++ GameVersion.cpp
@@ -13,11 +13,11 @@
 #ifdef JA2EDITOR
 
 //MAP EDITOR BUILD VERSION
-CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.4705 (Official Release)" }; 
+CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13.4870 (Official Release)" }; 
 #elif defined JA2BETAVERSION
 
 //BETA/TEST BUILD VERSION
-CHAR16 zVersionLabel[256]		= { L"Debug v1.13.4705 (Official Release)" }; 
+CHAR16 zVersionLabel[256]		= { L"Debug v1.13.4870 (Official Release)" }; 
 #elif defined CRIPPLED_VERSION
 
 //RELEASE BUILD VERSION s
@@ -26,10 +26,10 @@ CHAR16 zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION	
-	CHAR16		zVersionLabel[256]		= { L"Release v1.13.4705 (Official Release)" };
+	CHAR16		zVersionLabel[256]		= { L"Release v1.13.4870 (Official Release)" };
 #endif
 										
-CHAR8		czVersionNumber[16]	= { "Build 11.10.13" };		//YY.MM.DD
+CHAR8		czVersionNumber[16]	= { "Build 11.12.28" };		//YY.MM.DD
 CHAR16		zTrackingNumber[16]	= { L"Z" };
 	
 
