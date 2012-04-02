0333be5eb2207c6c7cf8d29929351d84809df14d
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2012-04-02 08:51:48 +0000

    - Updated VS 2010 project files to allow different ways for building the executable (by tazpn) o Take a look in the comment header for the "Language Defines.h" file for further informations o Enable PDF for Release builds, so crash dump is more effective (of course if we release a version we also have to release the PDB file)
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@5136 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Language Defines.h
+++ Language Defines.h
@@ -19,6 +19,65 @@
  */
 
 
+/* The recommend approach for VS2010 multi-language builds is to use the command line or the ja2.props file.
+
+    By default the language is ENGLISH and the Language Prefix is EN.
+
+	There are 3 ways you can build the JA2 1.13 executable file:
+
+	// -------------------------------------------------------
+	// 1. Using the command line
+	// -------------------------------------------------------
+
+    For example, where msbuild is located in %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\ if not on your path
+	msbuild.exe /p:Configuration=Release ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=PL /p:JA2Language=POLISH ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=DE /p:JA2Language=GERMAN ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=RU /p:JA2Language=RUSSIAN ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=NL /p:JA2Language=DUTCH ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=FR /p:JA2Language=FRENCH ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=IT /p:JA2Language=ITALIAN ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=CN /p:JA2Language=CHINESE ja2_VS2010.sln
+
+	Note: If you want to build "Unfinished Business" version, just append the /p:JA2Config=JA2UB in the command line
+	msbuild.exe /p:Configuration=Release /p:JA2Config=JA2UB ja2_VS2010.sln
+	msbuild.exe /p:Configuration=Release /p:JA2LangPrefix=DE /p:JA2Language=GERMAN /p:JA2Config=JA2UB ja2_VS2010.sln
+
+	// --------------------------------------------------------
+	// 2. Editing the ja2.props file and then build in VS 2010 
+	// -------------------------------------------------------
+	
+	1. Open the "ja2.props" file in a text editor and set the <BuildMacro> tags to your likeing.
+	
+	For example: If you want to build Russian Version (normal JA2, not UB) then set the following:
+
+	<BuildMacro Include="JA2Config">
+      <Value></Value>
+    </BuildMacro>
+    <BuildMacro Include="JA2LangPrefix">
+      <Value>RU</Value>
+    </BuildMacro>
+    <BuildMacro Include="JA2Language">
+      <Value>RUSSIAN</Value>
+    </BuildMacro>
+
+	2. Save the file
+	3. Build the project in Visual Studio 2010
+
+	// --------------------------------------------------------
+	// 3. The "old" way for building the executable
+	// -------------------------------------------------------
+
+	1. Enable the "#undef ENGLISH" define below, so English will not be used anymore
+	2. Set the desired language below
+	3. If you want to build "Unfinished Business" version, enable "#define JA2UB" and "#define JA2UBMAPS" in builddefines.h"
+	4. Build the executable in VS 2010
+	5. The output will be placed in the "Build\bin\VS2010" folder
+*/
+
+// Only enable this "undef", if you use the 3. way of building the executable!
+#undef ENGLISH
+
 #if !defined(ENGLISH) && !defined(GERMAN) && !defined(RUSSIAN) && !defined(DUTCH) && !defined(POLISH) && !defined(FRENCH) && !defined(ITALIAN) && !defined(TAIWANESE) && !defined(CHINESE)
 /* please set one manually here (by uncommenting) if not willingly to set Workspace wide */
 
