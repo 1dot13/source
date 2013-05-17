22bd1956f1649c58278b4c1e905fab4128a05099
Flugente <Flugente@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2013-05-17 18:05:18 +0000

    Fix: status bar of attached grenades was shown incorrectly
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@6083 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Tactical/Interface Utils.h
+++ Tactical/Interface Utils.h
@@ -3,11 +3,11 @@
 
 #include "Soldier Control.h"
 
+#define		DRAW_ITEM_TEMPERATURE				199		// Flugente: had to alter this value. Otherwise we might accidentally draw temeprature when we want to draw an attachments' status
 #define		DRAW_ITEM_STATUS_ATTACHMENT1		200
 #define		DRAW_ITEM_STATUS_ATTACHMENT2		201
 #define		DRAW_ITEM_STATUS_ATTACHMENT3		202
 #define		DRAW_ITEM_STATUS_ATTACHMENT4		203
-#define		DRAW_ITEM_TEMPERATURE				204		// Flugente FTW 1.2
 
 void DrawMoraleUIBarEx( SOLDIERTYPE *pSoldier, INT16 sXPos, INT16 sYPos, INT16 sWidth, INT16 sHeight, BOOLEAN fErase, UINT32 uiBuffer );
 void DrawBreathUIBarEx( SOLDIERTYPE *pSoldier, INT16 sXPos, INT16 sYPos, INT16 sWidth, INT16 sHeight, BOOLEAN fErase, UINT32 uiBuffer );
