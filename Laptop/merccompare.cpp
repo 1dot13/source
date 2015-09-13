6d08593bd04c884ad160ee19f01bf89d590c55a7
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2015-09-13 15:21:55 +0000

    MeLoDy Updates - Increased horizontal table column width to fit longer merc names
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@7991 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Laptop/merccompare.cpp
+++ Laptop/merccompare.cpp
@@ -1073,7 +1073,7 @@ void RenderMercCompareMatrix( )
 		}
 
 		UINT16 spacepermerc = (LAPTOP_SCREEN_LR_X - LAPTOP_SCREEN_UL_X) / (squadvector.size() + 1);
-		spacepermerc = min( spacepermerc , 70);
+		spacepermerc = min( spacepermerc , 65);
 
 		// now loop over the squadmembers and fill out the table
 		std::vector<UINT8>::iterator itend = squadvector.end( );
