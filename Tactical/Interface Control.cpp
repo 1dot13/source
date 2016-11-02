6c737fefd3371bc7ff6c6ea1ea307f4841a46b7e
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2016-11-02 13:24:17 +0000

    Fixed compilation errors in VS 2005: std::sqrt() -> sqrt()
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@8339 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Tactical/Interface Control.cpp
+++ Tactical/Interface Control.cpp
@@ -3,6 +3,7 @@
 #else
 	#include <stdio.h>
 	#include <time.h>
+	#include <math.h>
 	#include "sgp.h"
 	#include "gameloop.h"
 	#include "himage.h"
@@ -636,7 +637,7 @@ void DrawExplosionWarning( INT32 sGridno, INT8 usLevel, INT8 usDelay )
 		{
 			for ( INT32 y = yl; y <= yh; ++y )
 			{
-				FLOAT diff = std::sqrt( (FLOAT)((sScreenX - x) * (sScreenX - x) + 2 * (sScreenY - y) * (sScreenY - y)) );
+				FLOAT diff = sqrt( (FLOAT)((sScreenX - x) * (sScreenX - x) + 2 * (sScreenY - y) * (sScreenY - y)) );
 
 				if ( radius_inner <= diff && diff <= radius_outer )
 				{
