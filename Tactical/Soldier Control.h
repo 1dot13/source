dc4846a46496c696877aca3f2349d3db33aa1b9c
Flugente <Flugente@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2017-09-27 21:18:35 +0000

    Fix: as being covert breaks boxing fights, boxers are not covert
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@8481 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Tactical/Soldier Control.h
+++ Tactical/Soldier Control.h
@@ -641,7 +641,11 @@ extern CLOTHES_STRUCT Clothes[CLOTHES_MAX];
 // This macro should be used whenever we want to see if someone is neutral
 // IF WE ARE CONSIDERING ATTACKING THEM.	Creatures & bloodcats will attack neutrals
 // but they can't attack empty vehicles!!
-#define CONSIDERED_NEUTRAL( me, them ) ( (them->aiData.bNeutral || them->usSoldierFlagMask & (SOLDIER_COVERT_CIV|SOLDIER_COVERT_SOLDIER|SOLDIER_POW)) && ( me->bTeam != CREATURE_TEAM || (them->flags.uiStatusFlags & SOLDIER_VEHICLE) ) )
+#define CONSIDERED_NEUTRAL( me, them )  (\
+										( them->aiData.bNeutral || them->usSoldierFlagMask & (SOLDIER_COVERT_CIV|SOLDIER_COVERT_SOLDIER|SOLDIER_POW) ) \
+										&& ( me->bTeam != CREATURE_TEAM || (them->flags.uiStatusFlags & SOLDIER_VEHICLE) ) \
+										&& !( me->flags.uiStatusFlags & SOLDIER_BOXER || them->flags.uiStatusFlags & SOLDIER_BOXER ) \
+										)
 
 typedef struct
 {
