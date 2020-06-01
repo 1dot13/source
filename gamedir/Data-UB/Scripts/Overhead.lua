80f2f8e499852126edd49169e9cd235829bfd267
Flugente <Flugente@4f8fa57e-7814-0410-bad4-adc449f26b7c>
2020-06-01 22:05:55 +0000

    Fix: missing lua functions for UB
    git-svn-id: https://ja2svn.mooo.com/source/ja2_v1.13_data@2538 4f8fa57e-7814-0410-bad4-adc449f26b7c

--- gamedir/Data-UB/Scripts/Overhead.lua
+++ gamedir/Data-UB/Scripts/Overhead.lua
@@ -161,6 +161,84 @@ SectorY =
 	MAP_ROW_P = 16,
 }
 
+-- text colours
+FontColour =
+{
+	FONT_MCOLOR_DKWHITE = 134,
+	FONT_MCOLOR_LTYELLOW = 144,
+	FONT_MCOLOR_RED = 163,
+	FONT_MCOLOR_DKRED = 164,
+	FONT_MCOLOR_LTGREEN = 184,
+}
+
+-- these numbers aren't used in the code - we only use them in LUA
+Languages =
+{
+	LANGUAGE_ENGLISH = 0,
+	LANGUAGE_GERMAN = 1,
+	LANGUAGE_RUSSIAN = 2,
+	LANGUAGE_DUTCH = 3,
+	LANGUAGE_POLISH = 4,
+	LANGUAGE_FRENCH = 5,
+	LANGUAGE_ITALIAN = 6,
+	LANGUAGE_CHINESE = 7,
+}
+
+-- numbers for addressing which stat should get more experience
+StatTypes = 
+{	
+	HEALTHAMT = 1,
+	AGILAMT = 2,
+	DEXTAMT = 3,
+	WISDOMAMT = 4,
+	MEDICALAMT = 5,
+	EXPLODEAMT = 6,
+	MECHANAMT = 7,
+	MARKAMT = 8,
+	EXPERAMT = 9,
+	STRAMT = 10,
+	LDRAMT = 11,
+}
+
+-- different teams
+Teams =
+{
+	ENEMY_TEAM = 1,
+	CREATURE_TEAM = 2,
+	MILITIA_TEAM = 3,
+	CIV_TEAM = 4,
+}
+
+local iLoop
+local aimLoop
+
+local HEARD_3_TURNS_AGO	= -4
+local HEARD_2_TURNS_AGO	= -3
+local HEARD_LAST_TURN	=  -2
+local HEARD_THIS_TURN	= -1
+local NOT_HEARD_OR_SEEN	= 0
+local SEEN_CURRENTLY	= 1
+local SEEN_THIS_TURN	= 2
+local SEEN_LAST_TURN	= 3
+local SEEN_2_TURNS_AGO	=4
+local SEEN_3_TURNS_AGO = 5
+
+-- local function
+local function HandleJohnArrival( ID )
+
+	
+	
+end
+-- end local function
+
+-- local function
+local function HandleMaryArrival( ID )
+
+	
+
+end
+-- end local function
+
 function HandleAtNewGridNo( ProfileId )
 
 	if ( FindSoldierByProfileID (UB_GetTexID()) == UB_GetTexID() ) then
@@ -180,4 +258,219 @@ function HandleAtNewGridNo( ProfileId )
 
 	end -- End team
 
+end
+	
+ActionTypes =
+{
+	ACTIONTYPE_INVALID = 0,
+	ACTIONTYPE_HACKING = 1,
+	ACTIONTYPE_READFILE = 2,
+	ACTIONTYPE_WATERTAP = 3,
+	ACTIONTYPE_SODAMACHINE = 4,
+	ACTIONTYPE_MINIGAME = 5,
+}
+
+InfoTypes = 
+{	
+	INFO_TYPE_NORMAL = 0,
+	INFO_TYPE_VIP = 1,
+}
+
+ScreenTypes = 
+{
+	MINIGAME = 30,
+}
+
+MiniGames =
+{
+	TETRIS = 0,
+	PONG = 1,
+}
+
+-- We have an array of 1000 signed integers that a modder can use to set whatever data he wants.
+-- We simply set up some enums here to make it easier for us to remember what is what
+ModSpecificActions =
+{
+	
+}
+
+-- General defines for various states
+ActionState = 
+{
+	-- hacking
+	STATE_OK = 0,				-- starting state, we can hack this
+	STATE_END = 1,				-- computer is shut down and cannot be used
+	STATE_GAVEREWARD_OK = 2,	-- computer is still useable, we already got whatever reward there was
+	STATE_GAVEREWARD_END = 3,	-- computer is shut down and cannot be used, we already got whatever reward there was
+}
+
+-- handle the result of an interactive action that has a special id set for lua
+-- for more info, see InteractiveActions.xml
+function HandleInteractiveActionResult(sSectorX, sSectorY, bSectorZ, sGridNo, bLevel, ubID, usActionType, sLuaactionid, difficulty, skill )
+		
+	if ( usActionType == ActionTypes.ACTIONTYPE_HACKING ) then
+		
+	elseif ( usActionType == ActionTypes.ACTIONTYPE_READFILE ) then
+		
+	elseif ( usActionType == ActionTypes.ACTIONTYPE_MINIGAME ) then
+		
+	end
+	
+end
+
+-- states of a photo fact
+PhotoFlag = 
+{
+	NONE				= 0,
+	TAKEN 				= 1,
+	UPLOADED 			= 2,
+	VERIFIED 			= 3,	-- the code only has one value for verification, but we use several ones for different results
+	VERIFIED_RESULT_2 	= 4,
+}
+
+-- In this function we can define what and how many items a intel-based dealer should trade with, and how much an item is worth in intel
+-- AddArmsDealerAdditionalIntelData takes 4 arguments:
+-- usDealer: shopkeeper this is intended for
+-- usItem: number of item
+-- sIntelPrice: price of item in intel
+-- sOptimalNumber: how many items the trader should have at maximum
+function AddArmsDealerAdditionalIntelData()
+	
+end
+
+function SetPhotoState( aIndex, aState )
+	
+end
+
+-- sSectorX, sSectorY, bSectorZ: sector coordinates
+-- sGridNo: tile that we photographed. When we take a photo this function is run on all tiles we see in a 2-tile radius around where we clicked
+-- bLevel: height level (floor or roof)
+-- ubPhotographerProfile: profile of the merc taking the photo
+-- room: if > 0, this tile is inside this room
+-- usTargetProfile: if we photographed a NPC, this will be their profile number, otherwise NO_PROFILE
+function AddPhotoData( sSectorX, sSectorY, bSectorZ, sGridNo, bLevel, ubPhotographerProfile, room, usTargetProfile )
+	
+end
+
+function GetPhotoData( aType )
+	
+end
+
+function VerifyPhotoState(aIndex)
+	
+end
+
+-- types of weather
+Weather = 
+{
+	WEATHER_FORECAST_NORMAL = 0,
+	WEATHER_FORECAST_RAIN = 1,
+	WEATHER_FORECAST_THUNDERSHOWERS = 2,
+	WEATHER_FORECAST_SANDSTORM = 3,
+	WEATHER_FORECAST_SNOW = 4,
+};
+
+-- skills
+Skill =
+{		
+	-- radio operator
+	SKILLS_RADIO_ARTILLERY = 0,
+	SKILLS_RADIO_JAM = 1,
+	SKILLS_RADIO_SCAN_FOR_JAM = 2,
+	SKILLS_RADIO_LISTEN = 3,
+	SKILLS_RADIO_CALLREINFORCEMENTS = 4,
+	SKILLS_RADIO_TURNOFF = 5,
+
+	SKILLS_INTEL_CONCEAL = 6,
+	SKILLS_INTEL_GATHERINTEL = 7,
+	
+	-- various
+	SKILLS_SPOTTER = 8,
+	SKILLS_FOCUS = 9,
+	SKILLS_DRAG = 10,
+};
+
+-- different dialogue action events
+DialogueActionEvent = 
+{
+	ADE_DISCOVER_ROOM = 0,					-- merc is the 1st one to explore this room, aData1 indicates the room in this event
+	ADE_DIALOGUE_REACTION = 1,				-- someone else used a line, and we might answer to that, aData1 indicates the profile number in this event, aData2 indicates the specific voice line we react to, aData3 indicates whether this person has been recruited (1) or not(0)
+	ADE_MERC_ARRIVES = 2,					-- a mercenary arrives in Arulco. aData1 is the profile of the arriving merc, aData2 is 1 if the player hasn't made contact with the rebels yet
+	ADE_SECTOR_COMMENTARY = 3,				-- similar to the rebel's quotes, this is called upon visiting a sector for the first time (if no rebel does their quote first)
+	ADE_MERCHANT_CHAT = 4,					-- we greet a merchant (used with non-profile merchants, in order to be a bit more lively), aData1 is the id of the merchant
+	ADE_DIALOGUE_NPC_FRIENDLY = 5,			-- in a conversation, we can say something when clicking on a button
+	ADE_DIALOGUE_NPC_DIRECT = 6,
+	ADE_DIALOGUE_NPC_THREATEN = 7,
+	ADE_DIALOGUE_NPC_RECRUIT = 8,
+	ADE_DIALOGUE_NPC_REPEAT = 9,
+	ADE_DIALOGUE_RPC_RECRUIT_SUCCESS = 10,	-- we've successfully recruited an RPC, and might comment on that
+	ADE_SEX = 11,							-- does exactly what you think it does
+	ADE_WEATHERCHANGE = 12,					-- the weather has changed in the current sector, and we can warn the player about that, aData1 is type of Weather
+	ADE_SKILL_RESULT = 13,					-- we used a skill, and comment on the result, aData1 is of Skill, aData2 indicates whether it was a success (1) or failure (0)
+	ADE_GRENADEWARNING = 14,				-- a delayed enemy grenade was dropped nearby, and we can shout a warning, aData1 is the itemnumber of the grenade
+	ADE_CONSUMEITEM = 15,					-- we consumed an item, aData1 is the item index
+	ADE_NPC_DEATH = 16,						-- someone died, aData1 is the profile of the dead NPC, aData2 the profile of the killer, aData3 the bodytype of the dead
+	ADE_WITNESS_GOOD = 17,					-- similar to comment upon buddy doing good. aData1 is the profile of person, aData2 is 0 if we saw a kill, 1 if they passed a skill check (and we're less than 15 tiles away)
+	ADE_BANDAGE_PERFORM_BEGIN = 18,			-- we bandage someone, aData1 is the profile of person
+	ADE_BANDAGE_PERFORM_END = 19,			-- we finished bandaging someone, aData1 is the profile of person
+	ADE_BANDAGE_RECEIVE_BEGIN = 20,			-- someone bandages us, aData1 is the profile of person
+	ADE_BANDAGE_RECEIVE_END = 21,			-- someone finished bandaging us, aData1 is the profile of person
+	ADE_ZOMBIESRISE = 22,					-- a new wave of zombies is spawned from corpses
+	ADE_SEXUALHARASSMENT = 23,				-- the player wants to take the 'strip' command further than we are comfortable with
+	ADE_LOCKBOMB = 24,						-- merc uses a shaped charge to blow the locks of a door, aData1 indicates whether it was a success (1) or failure (0)
+	ADE_SNIPERWARNING = 25,					-- merc warns of sniper presence in this sector
+}
+
+-- functions used here:
+-- ubProfile: profile number of merc (don't change that unless you know what to do)
+-- iFaceIndex: index (don't change that unless you know what to do)
+-- path: path and name of soundfile we want to play (in .wav, .ogg or .mp3 format)
+-- text: "Text that should appear"
+-- SetAdditionalDialogue(ubProfile, iFaceIndex, path, text)
+-- Do not use this function in any lua calls outside of HandleAdditionalDialogue(..)!
+-- 
+-- path: path and name of soundfile we want to play (in .wav or .ogg format)
+-- volume: optional sound volume (65: medium volume, 127: loud), default 65
+-- PlaySound(path, volume)
+
+-- handle
+-- sSectorX, sSectorY and bSectorZ indicate the sector coordinates
+-- ubProfile is the merc for whom this is called
+-- iFaceIndex is the face of the merc. DON'T CHANGE THIS UNLESS YOU KNOW WHAT YOU ARE DOING!
+-- usEventNr indicates which event from DialogueActionEvent is used here
+-- aData1, aData2, aData3 are additional data, see event description on what they do in each event
+function HandleAdditionalDialogue(sSectorX, sSectorY, bSectorZ, ubProfile, iFaceIndex, usEventNr, aData1, aData2, aData3 )
+	
+end
+
+-- Look up voicelines in Dialogue Control.h, only listing notable ones often used here
+Voiceline = 
+{
+	QUOTE_SEE_ENEMY = 0,
+	QUOTE_KILLED_AN_ENEMY = 27,
+	
+	-- these three lines are played at random when we talk to one of our mercs. Despite the name they don't have any special meaning
+	QUOTE_NEGATIVE_COMPANY = 41,
+	QUOTE_SOCIAL_TRAIT = 44,
+	QUOTE_PASSING_DISLIKE = 45,
+	
+	QUOTE_ENEMY_PRESENCE = 59,
+	QUOTE_WARNING_OUTSTANDING_ENEMY_AFTER_RT = 60,
+	QUOTE_SECTOR_SAFE = 65,
+}
+
+-- This function is used to replace dialogue quotes with new ones. This way we can have more variety - a merc doesn't always use the same lines when something happens.
+-- If a line isn't replaced, it plays normally, so it is a good idea to not always replace lines.
+--
+-- functions used here:
+-- path: path and name of soundfile we want to play (in .wav, .ogg or .mp3 format)
+-- text: "Text that should appear"
+-- SetReplaceDialogue(path, text)
+-- Do not use this function in any lua calls outside of HandleReplaceQuote(..)!
+--
+-- handle
+-- ubProfile is the merc for whom this is called
+-- usQuoteNum is the quote that the game wants to play
+function HandleReplaceQuote( ubProfile, usQuoteNum )
+	
 end
--- gamedir/Data-UB/Scripts/strategicmap.lua
+++ gamedir/Data-UB/Scripts/strategicmap.lua
@@ -174,6 +174,100 @@ Profil =
 	MADLAB = 146,
 	ROBOT = 62,
 }
+
+Flags1 = 
+{
+	PROFILE_MISC_FLAG_RECRUITED = 1,
+	PROFILE_MISC_FLAG_HAVESEENCREATURE = 2,
+	PROFILE_MISC_FLAG_FORCENPCQUOTE = 4,
+	PROFILE_MISC_FLAG_WOUNDEDBYPLAYER = 8,
+	PROFILE_MISC_FLAG_TEMP_NPC_QUOTE_DATA_EXISTS = 16,
+	PROFILE_MISC_FLAG_SAID_HOSTILE_QUOTE = 32,
+	PROFILE_MISC_FLAG_EPCACTIVE = 64,
+	PROFILE_MISC_FLAG_ALREADY_USED_ITEMS = 128,
+}
+
+SoldierClass = 
+{                   
+    SOLDIER_CLASS_NONE = 0,
+	SOLDIER_CLASS_ADMINISTRATOR = 1,
+	SOLDIER_CLASS_ELITE = 2,
+	SOLDIER_CLASS_ARMY = 3,
+}
+
+CivGroup =
+{
+	REBEL_CIV_GROUP = 1,
+	KINGPIN_CIV_GROUP = 2,
+	WARDEN_CIV_GROUP = 11,
+	BOUNTYHUNTER_CIV_GROUP = 25,
+	SCIENTIST_GROUP = 27,
+	RADAR_TECHNICIAN_GROUP = 28,
+	AIRPORT_STAFF_GROUP = 29,
+	BARRACK_STAFF_GROUP = 30,
+	FACTORY_GROUP = 31,
+	ADMINISTRATIVE_STAFF_GROUP = 32,
+	LOYAL_CIV_GROUP = 33,
+	BLACKMARKET_GROUP = 34,
+}
+
+Bodytype = 
+{
+	REGMALE = 0,
+	BIGMALE = 1,
+	STOCKYMALE = 2,
+	REGFEMALE = 3,
+	FATCIV = 11,
+	MANCIV = 12,
+	MINICIV = 13,
+	DRESSCIV = 14,
+	HATKIDCIV = 15,
+	KIDCIV = 16,
+	CRIPPLECIV = 17,
+}
+
+Skin = 
+{
+	PINKSKIN = 0,
+	TANSKIN = 1,
+	DARKSKIN = 2,
+	BLACKSKIN = 3,
+}
+
+Hair = 
+{
+	WHITEHEAD = 0,
+	BLACKHEAD = 1,
+	BROWNHEAD = 2,
+	BLONDEHEAD = 3,
+	REDHEAD = 4,
+}
+
+Vest = 
+{
+	WHITEVEST = 0,
+	GYELLOWSHIRT = 1,
+	YELLOWVEST = 2,
+	GREYVEST = 3,
+	BROWNVEST = 4,
+	PURPLESHIRT = 5,
+	BLUEVEST = 6,
+	JEANVEST = 7,
+	GREENVEST = 8,
+	REDVEST = 9,
+	BLACKSHIRT = 10,
+}
+
+Pants = 
+{
+	BLUEPANTS = 0,
+	BLACKPANTS = 1,
+	JEANPANTS = 2,
+	TANPANTS = 3,
+	BEIGEPANTS = 4,
+	GREENPANTS = 5,
+}
+
 local gsRobotGridNo
 
 function HandleQuestCodeOnSectorEntry( sNewSectorX, sNewSectorY, bNewSectorZ )
@@ -259,4 +353,172 @@ local pSoldier
 		end
 	end
 
+end
+
+-- text colours
+FontColour =
+{
+	FONT_MCOLOR_DKWHITE = 134,
+	FONT_MCOLOR_LTYELLOW = 144,
+	FONT_MCOLOR_RED = 163,
+	FONT_MCOLOR_DKRED = 164,
+	FONT_MCOLOR_LTGREEN = 184,
+}
+
+-- these numbers aren't used in the code - we only use them in LUA
+Languages =
+{
+	LANGUAGE_ENGLISH = 0,
+	LANGUAGE_GERMAN = 1,
+	LANGUAGE_RUSSIAN = 2,
+	LANGUAGE_DUTCH = 3,
+	LANGUAGE_POLISH = 4,
+	LANGUAGE_FRENCH = 5,
+	LANGUAGE_ITALIAN = 6,
+	LANGUAGE_CHINESE = 7,
+}
+
+-- We have an array of 1000 signed integers that a modder can use to set whatever data he wants.
+-- We simply set up some enums here to make it easier for us to remember what is what
+ModSpecificFacts =
+{
+	-- |||||||||||||||||||||||||||||||||| factories |||||||||||||||||||||||||||||||||||||
+	-- |||||||||||||||||||||||||||||||||| factories |||||||||||||||||||||||||||||||||||||
+}
+
+FactorySpecialValues =
+{
+	FACTORY_PROGRESS_DRASSEN_TSHIRTFACTORY_UPGRADE = 6,
+}
+
+-- sSectorX, sSectorY and bSectorZ indicate the sector coordinates
+-- usFacilityType is facility number from FacilitTypes.xml
+-- usProductionNumber denotes which FACTORY of the facility this is for
+-- sProgressLeft is the progress to be saved. 
+--
+-- As factories can be added or removed in the xml at will, we can't hardcode their progress in the savegame.
+-- Therefore we let the modder store their progress in here via LUAFacts into the modder-administered part of the savefile.
+-- We also want factories to be deactivated initially (so the player doesn't suddenly lose money if he takes their sector). Initially all values are 0.
+-- In the code, values < 0 indicate a factory is offline, >= 0 online.
+-- We thus add '1' to every value, so we store the progress as 1 + sProgressLeft, this means a luafact value of <= 0 is offline, > 0 is online
+--
+-- We also use the Getter to check for other conditions, like quest progress. For example, even if we control Drassen, we can only use the T-Shirt factory once Doreen is gone.
+-- We achieve that by returning a value < -10 if these extra conditions are not satisfied.
+-- The code checks that too and won't allow us to even turn a factory on in this case, so the player knows he has something else to do first.
+function SetFactoryLeftoverProgress(sSectorX, sSectorY, bSectorZ, usFacilityType, usProductionNumber, sProgressLeft)
+	
+end
+
+function GetFactoryLeftoverProgress(sSectorX, sSectorY, bSectorZ, usFacilityType, usProductionNumber, sProgressLeft)
+	
+	CANT_ACTIVATE_FACTORY = -20
+	
+	val = -1
+	
+	return val
+
+end
+
+-- this function is called whenever we liberate a sector. If fFirstTime is true, this is the first time we liberate this sector
+function HandleSectorLiberation( sNewSectorX, sNewSectorY, bNewSectorZ, fFirstTime )
+	
+end
+
+-- this function is called whenever we recruit a RPC
+function RecruitRPCAdditionalHandling( usProfile )
+	
+end
+
+-- this function is called whenever we enter a sector in tactical
+function HandleSectorTacticalEntry( sSectorX, sSectorY, bSectorZ, fHasEverBeenPlayerControlled )
+	
+end
+
+-- these colours can be used in the map
+-- if you want to know how they look, test them on the map
+-- if you want more colours, check whether you feel lucky and ask a coder
+MapColour = {
+	MAP_SHADE_BLACK = 0,
+	
+	MAP_SHADE_LT_GREEN = 1,
+	MAP_SHADE_MD_GREEN = 2,
+	MAP_SHADE_DK_GREEN = 3,
+	
+	MAP_SHADE_LT_RED = 4,
+	MAP_SHADE_MD_RED = 5,
+	MAP_SHADE_DK_RED = 6,
+	
+	MAP_SHADE_LT_YELLOW = 7,
+	MAP_SHADE_MD_YELLOW = 8,
+	MAP_SHADE_DK_YELLOW = 9,
+	
+	MAP_SHADE_LT_CYAN = 10,
+	MAP_SHADE_MD_CYAN = 11,
+	MAP_SHADE_DK_CYAN = 12,
+	
+	MAP_SHADE_LT_GREY = 13,
+	MAP_SHADE_MD_GREY = 14,
+	MAP_SHADE_DK_GREY = 15,
+	
+	MAP_SHADE_LT_BLUE = 16,
+	MAP_SHADE_MD_BLUE = 17,
+	MAP_SHADE_DK_BLUE = 18,
+	
+	MAP_SHADE_LT_PINK = 19,
+	MAP_SHADE_ORANGE = 20,
+	
+	MAP_SHADE_MIX_RED = 21,
+	MAP_SHADE_MIX_GREEN = 22,
+	MAP_SHADE_MIX_BLUE = 23,
+	MAP_SHADE_MIX_YELLOW = 24,
+	
+	MAP_SHADE_MIX_RED_GREEN = 25,
+	MAP_SHADE_MIX_RED_BLUE = 26,
+	MAP_SHADE_MIX_RED_YELLOW = 27,
+	MAP_SHADE_MIX_GREEN_BLUE = 28,
+	MAP_SHADE_MIX_GREEN_YELLOW = 29,
+	MAP_SHADE_MIX_BLUE_YELLOW = 30,
+	
+	MAP_SHADE_MIX_RED_GREEN_BLUE = 31,
+	MAP_SHADE_MIX_RED_GREEN_YELLOW = 32,
+	MAP_SHADE_MIX_RED_BLUE_YELLOW = 33,
+	
+	MAP_SHADE_MIX_GREEN_BLUE_YELLOW = 34,
+	MAP_SHADE_MIX_RED_GREEN_BLUE_YELLOW = 35,
+}
+
+
+-- These symbols are stored in Interface/IntelMapSymbols.sti
+-- if you want more symbols, add them
+MapSymbols = {
+	BLOODCAT_ATTACK = 0,
+	ZOMBIE_ATTACK = 1,
+	BANDIT_ATTACK = 2,
+	TERRORIST_LOCATION = 3,
+	CHALICE = 4,
+	ASSASSINATION_TARGET = 5,
+	HELI = 6,
+	PRISON = 7,
+	CHURCH = 8,	
+	RECRUITMENT = 9,
+	FLAG = 10,
+	QUESTIONMARK_BLUE = 11,		-- sector might be relevant for a quest
+	EXCLAMATIONMARK_BLUE = 12,	-- sector is definetely relevant for a quest	
+	QUESTIONMARK_GREEN = 13,	-- alternate colours for other uses
+	EXCLAMATIONMARK_GREEN = 14,
+	QUESTIONMARK_RED = 15,
+	EXCLAMATIONMARK_RED = 16,
+	QUESTIONMARK_YELLOW = 17,
+	EXCLAMATIONMARK_YELLOW = 18,
+	FACTORY_YELLOW = 19,
+	FACTORY_GREEN = 20,
+	FACTORY_RED = 21,
+	FACTORY_NOCONTROL = 22,
+}
+
+-- this function allows us to data for the intel/quest map
+-- aLevel is the world level (0: surface)
+-- set data by using SetIntelAndQuestMapDataForSector(sectorx, sextory, MapColour, symbol, text for description window, short text for map)
+function GetIntelAndQuestMapData( aLevel )
+	
 end
