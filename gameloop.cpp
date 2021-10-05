712d6664be63056edaf4384d126efb799d9efa5e
Shadooow <Shadooow@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2021-10-05 02:31:58 +0000

    removed one of my comments that got into slive ource by accident
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@9173 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- gameloop.cpp
+++ gameloop.cpp
@@ -421,7 +421,7 @@ void GameLoop(void)
 	// Flugente: dynamic opinions: Dialogue Boxes need to be refreshed
 	if (gGameExternalOptions.fDynamicOpinions)
 	{
-		RefreshBoxes();//todo shadooow: tohle mozna zpusobuje bug s opakovanim kecu po vitezstvi
+		RefreshBoxes();
 	}
 	
 	// Flugente:disguised mercs can distract enemies by talking to them. In order to display that, we sometimes display excerpts of their 'chats'
