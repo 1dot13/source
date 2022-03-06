1dot13/source

commit 539de4e83835c683755598d7178a03a23f35bb57

Fixed dead enemies after autoresolve sometimes being placed on non-walkable grids which, if they dropped any items, made such items inaccessible as well.
git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@9305 3b4a5df2-a311-0410-b5c6-a8a6f20db521

TileEngine/Isometric Utils.cpp
@@ -1277,7 +1277,15 @@ INT16 MapY( INT32 sGridNo )
 	return( sYPos );
 }
 
-
+bool GridNoOnWalkableWorldTile(INT32 sGridNo)
+{
+	MAP_ELEMENT *pMapElement = &(gpWorldLevelData[sGridNo]);
+	//Shadooow: as far as I know, Drassen Mine (tileset id 23) is the only map where the base land height isn't 0
+	//if there are more, add them into this hardcoded check
+	UINT8 sDefaultTilesetLandHeight = giCurrentTilesetID == 23 ? 80 : 0;
+	//ScreenMsg(FONT_MCOLOR_LTYELLOW, MSG_INTERFACE, L"GridNoOnWalkableWorldTile, tileset id: %i, gridno height level: %i", giCurrentTilesetID, pMapElement->sHeight);
+	return pMapElement->sHeight == sDefaultTilesetLandHeight;
+}
 
 BOOLEAN GridNoOnVisibleWorldTile( INT32 sGridNo )
 {
@@ -1296,7 +1304,7 @@ BOOLEAN GridNoOnVisibleWorldTile( INT32 sGridNo )
 	if ( sWorldX >= 30 && sWorldX <= (gsTRX - gsTLX - 30) && sWorldY >= 20 && sWorldY <= (gsBLY - gsTLY - 10) )
 #endif
 	{
-		return( TRUE );
+		return GridNoOnWalkableWorldTile(sGridNo);
 	}
 
 	return( FALSE );

commit 0d5c1be5d4b201da55c7ad91e5224825f6d59fde

Fixed regression caused by r9305 where maps in some mods like AIMNAS became unwalkable completely.
git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@9317 3b4a5df2-a311-0410-b5c6-a8a6f20db521

TileEngine/Isometric Utils.cpp
@@ -11,7 +11,7 @@
 	#include "Random.h"
 	#include "Pathai.h"
 #endif
-
+#include "Map Information.h"
 
 UINT32 guiForceRefreshMousePositionCalculation = 0;
 
@@ -1279,12 +1279,10 @@ INT16 MapY( INT32 sGridNo )
 
 bool GridNoOnWalkableWorldTile(INT32 sGridNo)
 {
+	//Shadooow: this will compare sGridNo height with height of the center grid of the map, as long as the center of the map is on a walkable height it will work properly
 	MAP_ELEMENT *pMapElement = &(gpWorldLevelData[sGridNo]);
-	//Shadooow: as far as I know, Drassen Mine (tileset id 23) is the only map where the base land height isn't 0
-	//if there are more, add them into this hardcoded check
-	UINT8 sDefaultTilesetLandHeight = giCurrentTilesetID == 23 ? 80 : 0;
-	//ScreenMsg(FONT_MCOLOR_LTYELLOW, MSG_INTERFACE, L"GridNoOnWalkableWorldTile, tileset id: %i, gridno height level: %i", giCurrentTilesetID, pMapElement->sHeight);
-	return pMapElement->sHeight == sDefaultTilesetLandHeight;
+	MAP_ELEMENT *pMapElementCenter = &(gpWorldLevelData[gMapInformation.sCenterGridNo]);
+	return pMapElement->sHeight == pMapElementCenter->sHeight;
 }
 
 BOOLEAN GridNoOnVisibleWorldTile( INT32 sGridNo )