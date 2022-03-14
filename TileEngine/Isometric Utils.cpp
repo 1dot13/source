f360fcbf2255fbb33b8ccda7ab28f17367ad1eb3
Shadooow <Shadooow@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2022-03-14 10:16:46 +0000

    Fixed regression causing cutscenes with deidranna going into endless clocks.
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk/GameSource/ja2_v1.13/Build@9320 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- TileEngine/Isometric Utils.cpp
+++ TileEngine/Isometric Utils.cpp
@@ -12,6 +12,7 @@
 	#include "Pathai.h"
 #endif
 #include "Map Information.h"
+#include "meanwhile.h"
 
 UINT32 guiForceRefreshMousePositionCalculation = 0;
 
@@ -1279,6 +1280,7 @@ INT16 MapY( INT32 sGridNo )
 
 bool GridNoOnWalkableWorldTile(INT32 sGridNo)
 {
+	if (AreInMeanwhile()) return true;
 	//Shadooow: this will compare sGridNo height with height of the center grid of the map, as long as the center of the map is on a walkable height it will work properly
 	MAP_ELEMENT *pMapElement = &(gpWorldLevelData[sGridNo]);
 	MAP_ELEMENT *pMapElementCenter = &(gpWorldLevelData[gMapInformation.sCenterGridNo]);
