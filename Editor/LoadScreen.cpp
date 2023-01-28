b4da351dd2b6418448a8dc7a0d6078cb0db14f1a
Marco Antonio Jaguaribe Costa <marco.antonio.costa@gmail.com>
2023-01-27 21:05:11 -0300

    replace header preprocessor conditional with cpp function

--- Editor/LoadScreen.cpp
+++ Editor/LoadScreen.cpp
@@ -562,7 +562,7 @@ void CreateFileDialog( STR16 zTitle )
 	//File list window
 	iFileDlgButtons[4] = CreateHotSpot( (iScreenWidthOffset + 179+4), (iScreenHeightOffset + 69+3), (179+4+240), (69+120+3), MSYS_PRIORITY_HIGH-1, BUTTON_NO_CALLBACK, FDlgNamesCallback);	
 	//Title button
-	iFileDlgButtons[5] = CreateTextButton(zTitle, HUGEFONT, FONT_LTKHAKI, FONT_DKKHAKI,
+	iFileDlgButtons[5] = CreateTextButton(zTitle, GetHugeFont(), FONT_LTKHAKI, FONT_DKKHAKI,
 		BUTTON_USE_DEFAULT,iScreenWidthOffset + 179, iScreenHeightOffset + 39,281,30,BUTTON_NO_TOGGLE,
 		MSYS_PRIORITY_HIGH-2,BUTTON_NO_CALLBACK,BUTTON_NO_CALLBACK);
 	DisableButton(iFileDlgButtons[5]);
@@ -1013,7 +1013,7 @@ UINT32 ProcessFileIO()
 		case INITIATE_MAP_SAVE:	//draw save message 
 			StartFrameBufferRender( );
 			SaveFontSettings();
-			SetFont( HUGEFONT );
+			SetFont( GetHugeFont() );
 			SetFontForeground( FONT_LTKHAKI );
 			SetFontShadow( FONT_DKKHAKI );
 			SetFontBackground( 0 );
