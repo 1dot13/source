32777afee77fca819b594c9edc28f0aa7c453c3f
rftrdev <102184004+rftrdev@users.noreply.github.com>
2023-07-19 18:00:09 -0700

    Update growth modifier randomisation

--- Tactical/Soldier Profile.cpp
+++ Tactical/Soldier Profile.cpp
@@ -472,38 +472,60 @@ void RandomGrowthModifiers()
 		{
 			pProfile = &(gMercProfiles[cnt]);
 
-			if (pProfile->bGrowthModifierExpLevel != 0)
-				pProfile->bGrowthModifierExpLevel		= RandomAbsoluteRange( pProfile->bGrowthModifierExpLevel, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierExpLevel > 0)
+				pProfile->bGrowthModifierExpLevel		= RandomAbsoluteRange( pProfile->bGrowthModifierExpLevel, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierExpLevel < 0)
+				pProfile->bGrowthModifierExpLevel		= RandomAbsoluteRange( pProfile->bGrowthModifierExpLevel, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierLife != 0)
-				pProfile->bGrowthModifierLife			= RandomAbsoluteRange( pProfile->bGrowthModifierLife, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierLife > 0)
+				pProfile->bGrowthModifierLife			= RandomAbsoluteRange( pProfile->bGrowthModifierLife, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierLife < 0)
+				pProfile->bGrowthModifierLife			= RandomAbsoluteRange( pProfile->bGrowthModifierLife, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierAgility != 0)
-				pProfile->bGrowthModifierAgility		= RandomAbsoluteRange( pProfile->bGrowthModifierAgility, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierAgility > 0)
+				pProfile->bGrowthModifierAgility		= RandomAbsoluteRange( pProfile->bGrowthModifierAgility, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierAgility < 0)
+				pProfile->bGrowthModifierAgility		= RandomAbsoluteRange( pProfile->bGrowthModifierAgility, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierDexterity != 0)
-				pProfile->bGrowthModifierDexterity		= RandomAbsoluteRange( pProfile->bGrowthModifierDexterity, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierDexterity > 0)
+				pProfile->bGrowthModifierDexterity		= RandomAbsoluteRange( pProfile->bGrowthModifierDexterity, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierDexterity < 0)
+				pProfile->bGrowthModifierDexterity		= RandomAbsoluteRange( pProfile->bGrowthModifierDexterity, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierStrength != 0)
-				pProfile->bGrowthModifierStrength		= RandomAbsoluteRange( pProfile->bGrowthModifierStrength, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierStrength > 0)
+				pProfile->bGrowthModifierStrength		= RandomAbsoluteRange( pProfile->bGrowthModifierStrength, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierStrength < 0)
+				pProfile->bGrowthModifierStrength		= RandomAbsoluteRange( pProfile->bGrowthModifierStrength, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierLeadership != 0)
-				pProfile->bGrowthModifierLeadership		= RandomAbsoluteRange( pProfile->bGrowthModifierLeadership, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierLeadership > 0)
+				pProfile->bGrowthModifierLeadership		= RandomAbsoluteRange( pProfile->bGrowthModifierLeadership, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierLeadership < 0)
+				pProfile->bGrowthModifierLeadership		= RandomAbsoluteRange( pProfile->bGrowthModifierLeadership, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierWisdom != 0)
-				pProfile->bGrowthModifierWisdom			= RandomAbsoluteRange( pProfile->bGrowthModifierWisdom, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierWisdom > 0)
+				pProfile->bGrowthModifierWisdom			= RandomAbsoluteRange( pProfile->bGrowthModifierWisdom, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierWisdom < 0)
+				pProfile->bGrowthModifierWisdom			= RandomAbsoluteRange( pProfile->bGrowthModifierWisdom, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierMarksmanship != 0)
-				pProfile->bGrowthModifierMarksmanship	= RandomAbsoluteRange( pProfile->bGrowthModifierMarksmanship, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierMarksmanship > 0)
+				pProfile->bGrowthModifierMarksmanship	= RandomAbsoluteRange( pProfile->bGrowthModifierMarksmanship, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierMarksmanship < 0)
+				pProfile->bGrowthModifierMarksmanship	= RandomAbsoluteRange( pProfile->bGrowthModifierMarksmanship, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierMechanical != 0)
-				pProfile->bGrowthModifierMechanical		= RandomAbsoluteRange( pProfile->bGrowthModifierMechanical, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierMechanical > 0)
+				pProfile->bGrowthModifierMechanical		= RandomAbsoluteRange( pProfile->bGrowthModifierMechanical, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierMechanical < 0)
+				pProfile->bGrowthModifierMechanical		= RandomAbsoluteRange( pProfile->bGrowthModifierMechanical, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierExplosive != 0)
-				pProfile->bGrowthModifierExplosive		= RandomAbsoluteRange( pProfile->bGrowthModifierExplosive, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierExplosive > 0)
+				pProfile->bGrowthModifierExplosive		= RandomAbsoluteRange( pProfile->bGrowthModifierExplosive, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierExplosive < 0)
+				pProfile->bGrowthModifierExplosive		= RandomAbsoluteRange( pProfile->bGrowthModifierExplosive, -500, 0, 50, useBellCurve );
 
-			if (pProfile->bGrowthModifierMedical != 0)
-				pProfile->bGrowthModifierMedical		= RandomAbsoluteRange( pProfile->bGrowthModifierMedical, -500, 500, 50, useBellCurve );
+			if (pProfile->bGrowthModifierMedical > 0)
+				pProfile->bGrowthModifierMedical		= RandomAbsoluteRange( pProfile->bGrowthModifierMedical, 0, 500, 50, useBellCurve );
+			else if (pProfile->bGrowthModifierMedical < 0)
+				pProfile->bGrowthModifierMedical		= RandomAbsoluteRange( pProfile->bGrowthModifierMedical, -500, 0, 50, useBellCurve );
 		}
 	}
 }
