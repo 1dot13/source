6d506499015ada1527a9ebd667146a0291168652
rftrdev <102184004+rftrdev@users.noreply.github.com>
2023-07-29 00:35:23 -0700

    Update growth modifiers random range from [0,500] to [-50,50]

--- GameSettings.cpp
+++ GameSettings.cpp
@@ -1080,7 +1080,7 @@ void LoadGameExternalOptions()
 	gGameExternalOptions.fMercRandomStartSalary				= iniReader.ReadBoolean("Recruitment Settings", "MERCS_RANDOM_START_SALARY", FALSE);
 	gGameExternalOptions.ubMercRandomStartSalaryPercentMod	= iniReader.ReadInteger("Recruitment Settings", "MERCS_RANDOM_START_SALARY_PERCENTAGE_MAX_MODIFIER", 30, 0, 100);
 	gGameExternalOptions.fMercRandomGrowthModifiers			= iniReader.ReadBoolean("Recruitment Settings", "MERCS_RANDOM_GROWTH_MODIFIERS", FALSE);
-	gGameExternalOptions.iMercRandomGrowthModifiersRange	= iniReader.ReadInteger("Recruitment Settings", "MERCS_RANDOM_GROWTH_MODIFIERS_RANGE", 100, 0, 500);
+	gGameExternalOptions.iMercRandomGrowthModifiersRange	= iniReader.ReadInteger("Recruitment Settings", "MERCS_RANDOM_GROWTH_MODIFIERS_RANGE", 5, -50, 50);
 
 	//################# Financial Settings #################
 
