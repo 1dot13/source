0c3ca15558059011e49f9737ede8e493468e05a3
Andrzej Fałkowski <vkowsky@gmail.com>
2023-10-02 18:01:09 +0200

    Fixed UI movement mode not switching to crawl on character collapse, leading to deadlocks

--- Tactical/Points.cpp
+++ Tactical/Points.cpp
@@ -3214,7 +3214,7 @@ BOOLEAN CheckForMercContMove( SOLDIERTYPE *pSoldier )
 		return( FALSE );
 	}
 
-	if( pSoldier->stats.bLife >= OKLIFE )
+	if( pSoldier->stats.bLife >= OKLIFE && !(pSoldier->bCollapsed && pSoldier->bBreath < OKBREATH) )
 	{
 		if( pSoldier->sGridNo != pSoldier->pathing.sFinalDestination || pSoldier->bGoodContPath	)
 		{
--- Tactical/Soldier Control.cpp
+++ Tactical/Soldier Control.cpp
@@ -21837,6 +21837,8 @@ void SoldierCollapse( SOLDIERTYPE *pSoldier )
 
 	pSoldier->bCollapsed = TRUE;
 
+	pSoldier->usUIMovementMode = CRAWLING;
+
 	pSoldier->ReceivingSoldierCancelServices( );
 
 	// CC has requested - handle sight here...
