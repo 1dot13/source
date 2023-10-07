b68f206969e14c03a9c7deac2a45d84375a6fafa
Marco Antonio Jaguaribe Costa <marco.antonio.costa@gmail.com>
2023-10-07 08:29:59 -0300

    remove annoying warning on MSVC 19.37.32822
    `register` keyword does exactly nothing:
    https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/c5033?view=msvc-170

--- Tactical/DisplayCover.cpp
+++ Tactical/DisplayCover.cpp
@@ -373,7 +373,7 @@ BOOLEAN HasAdjTile( const INT32& ubX, const INT32& ubY, const INT32& ubZ )
 
 void AddCoverObjectsToViewArea()
 {
-	register INT32 ubX, ubY, ubZ;
+	INT32 ubX, ubY, ubZ;
 	BOOLEAN fChanged = FALSE;
 
 	BOOLEAN fNightTime = NightTime( );
@@ -406,7 +406,7 @@ void RemoveCoverObjectsFromViewArea()
 		return;
 
 	// Go through the whole gCoverViewArea when removing cover objects. Otherwise we don't clean up tiles that are not in the viewport which results in an annoying visual bug.
-	register INT32 ubX, ubY, ubZ;
+	INT32 ubX, ubY, ubZ;
 	BOOLEAN fChanged = FALSE;
 
 	for ( ubX = 0; ubX < COVER_X_CELLS; ++ubX )
@@ -669,8 +669,8 @@ static void CalculateCoverFromEnemies()
 
 void CalculateCover()
 {
-	register INT32 ubX, ubY;
-	register INT8 ubZ;
+	INT32 ubX, ubY;
+	INT8 ubZ;
 	SOLDIERTYPE* pSoldier;
 
 	if (gusSelectedSoldier == NOBODY)
@@ -1144,7 +1144,7 @@ void AddMinesObjectsToViewArea()
 	if ( gsMaxCellY < 0 )
 		return;
 	
-	register INT32 ubX, ubY, ubZ;
+	INT32 ubX, ubY, ubZ;
 	BOOLEAN fChanged = FALSE;
 
 	BOOLEAN fNightTime = NightTime( );
@@ -1503,7 +1503,7 @@ void AddTraitObjectsToViewArea()
 	if ( gsMaxCellY < 0 )
 		return;
 	
-	register INT32 ubX, ubY, ubZ;
+	INT32 ubX, ubY, ubZ;
 	BOOLEAN fChanged = FALSE;
 
 	BOOLEAN fNightTime = NightTime( );
@@ -1657,7 +1657,7 @@ void AddTrackerObjectsToViewArea( )
 	if ( gsMaxCellY < 0 )
 		return;
 
-	register INT32 ubX, ubY, ubZ;
+	INT32 ubX, ubY, ubZ;
 	BOOLEAN fChanged = FALSE;
 
 	BOOLEAN fNightTime = NightTime();
@@ -1731,7 +1731,7 @@ void CalculateFortify( )
 	if ( gsMaxCellY < 0 )
 		return;
 
-	register INT32 ubX, ubY, ubZ;
+	INT32 ubX, ubY, ubZ;
 	BOOLEAN fChanged = FALSE;
 
 	BOOLEAN fNightTime = NightTime( );
