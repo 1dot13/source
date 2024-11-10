040410b450a5b4df4fa8c9eab09c905921da8dbd
Asdow <20314541+Asdow@users.noreply.github.com>
2024-11-10 22:03:46 +0200

    Whitespace formatting for SAVED_GAME_HEADER

--- Ja2/SaveLoadGame.h
+++ Ja2/SaveLoadGame.h
@@ -38,41 +38,29 @@ class SOLDIERTYPE;
 
 typedef struct
 {
-	UINT32	uiSavedGameVersion;
-	CHAR8		zGameVersionNumber[ GAME_VERSION_LENGTH ];
-
-	CHAR16	sSavedGameDesc[ SIZE_OF_SAVE_GAME_DESC ];
-
-	UINT32	uiFlags;
-
+	UINT32			uiSavedGameVersion;
+	CHAR8			zGameVersionNumber[ GAME_VERSION_LENGTH ];
+	CHAR16			sSavedGameDesc[ SIZE_OF_SAVE_GAME_DESC ];
+	UINT32			uiFlags;
 #ifdef CRIPPLED_VERSION
-	UINT8 ubCrippleFiller[20];
+	UINT8			ubCrippleFiller[20];
 #endif
-
-
 	//The following will be used to quickly access info to display in the save/load screen
-	UINT32	uiDay;
-	UINT8		ubHour;
-	UINT8		ubMin;
-	INT16		sSectorX;
-	INT16		sSectorY;
-	INT8		bSectorZ;
-	UINT16		ubNumOfMercsOnPlayersTeam;
-	INT32		iCurrentBalance;
-
-	UINT32	uiCurrentScreen;
-
-	BOOLEAN	fAlternateSector;
-
-	BOOLEAN	fWorldLoaded;
-
-	UINT8		ubLoadScreenID;		//The load screen that should be used when loading the saved game
-
-	GAME_OPTIONS	sInitialGameOptions;	//need these in the header so we can get the info from it on the save load screen.
-
-	UINT32	uiRandom;
-
-	UINT8		ubFiller[494];	// WANNE: Decrease this filler by 1, for each new UINT8 variable!
+	UINT32			uiDay;
+	UINT8			ubHour;
+	UINT8			ubMin;
+	INT16			sSectorX;
+	INT16			sSectorY;
+	INT8				bSectorZ;
+	UINT16			ubNumOfMercsOnPlayersTeam;
+	INT32			iCurrentBalance;
+	UINT32			uiCurrentScreen;
+	BOOLEAN			fAlternateSector;
+	BOOLEAN			fWorldLoaded;
+	UINT8			ubLoadScreenID;		//The load screen that should be used when loading the saved game
+	GAME_OPTIONS		sInitialGameOptions;	//need these in the header so we can get the info from it on the save load screen.
+	UINT32			uiRandom;
+	UINT8			ubFiller[494];	// WANNE: Decrease this filler by 1, for each new UINT8 variable!
 
 } SAVED_GAME_HEADER;
 
