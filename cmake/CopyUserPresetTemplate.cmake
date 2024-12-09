b6572e2e63bda26730229e92f704ec555d4e9614
Marco Antonio Jaguaribe Costa <marco.antonio.costa@gmail.com>
2024-12-09 13:52:14 -0300

    (actually) fix broken MS2022 behavior
    a recent Visual Studio update introduced an error unless there is a
    CMakePresets.json file present, completely blowing up the previous
    setup.
    
    after this commit, you should either delete the CMakeUserPresets.json
    file from your root or add it to your system - NOT repo! - excludesFile:
    https://stackoverflow.com/questions/7335420/global-git-ignore

--- cmake/CopyUserPresetTemplate.cmake
+++ cmake/CopyUserPresetTemplate.cmake
@@ -5,7 +5,7 @@ function(CopyUserPresetTemplate)
 		NOT EXISTS "${CMAKE_SOURCE_DIR}/CMakePresets.json" AND
 		NOT EXISTS "${CMAKE_SOURCE_DIR}/CMakeUserPresets.json"
 	)
-		file(COPY "${CMAKE_SOURCE_DIR}/cmake/presets/CMakeUserPresets.json" DESTINATION "${CMAKE_SOURCE_DIR}")
-		message(FATAL_ERROR "No existing preset was found, copied a preset template to ${CMAKE_SOURCE_DIR}/CMakeUserPresets.json.")
+		file(COPY "${CMAKE_SOURCE_DIR}/cmake/presets/CMakePresets.json" DESTINATION "${CMAKE_SOURCE_DIR}")
+		message(FATAL_ERROR "No existing preset was found, copied a preset template to ${CMAKE_SOURCE_DIR}/CMakePresets.json.")
 	endif()
 endfunction()
