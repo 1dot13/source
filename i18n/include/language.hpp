ae7353904fd13d1111dddc97801c3bf9d25f069b
Marco Antonio Jaguaribe Costa <marco.antonio.costa@gmail.com>
2024-12-24 10:45:04 -0300

    add an internationalization constant
    the idea is to pluck all the language preprocessor nastiness to this single library from everywhere else
    
    once it's just here it's easy to not use preprocessor definitions at all

---
+++ i18n/include/language.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace i18n {
+enum class Lang
+{
+  en,
+  zh,
+  nl,
+  fr,
+  de,
+  it,
+  pl,
+  ru
+};
+}
+
+/* FIXME: The ugliest of ugly hacks. Getting rid of this and letting language
+ * (ideally text and voice separately) be set in the options menu would be
+ * ideal. */
+inline constexpr i18n::Lang g_lang
+{
+#if defined(ENGLISH)
+  i18n::Lang::en
+#elif defined(CHINESE)
+  i18n::Lang::zh
+#elif defined(DUTCH)
+  i18n::Lang::nl
+#elif defined(FRENCH)
+  i18n::Lang::fr
+#elif defined(GERMAN)
+  i18n::Lang::de
+#elif defined(ITALIAN)
+  i18n::Lang::it
+#elif defined(POLISH)
+  i18n::Lang::pl
+#elif defined(RUSSIAN)
+  i18n::Lang::ru
+#endif
+};
