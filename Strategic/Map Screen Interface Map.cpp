1aca65a6ff6433e712f0dc3ac68926f6a09cdf3f
BeatAroundTheBuscher <81776525+BeatAroundTheBuscher@users.noreply.github.com>
2025-07-24 14:30:19 +0200

    Update Map Screen Interface Map.cpp - Fix Encoding

--- Strategic/Map Screen Interface Map.cpp
+++ Strategic/Map Screen Interface Map.cpp
@@ -1201,7 +1201,7 @@ DebugMsg (TOPIC_JA2,DBG_LEVEL_3,"Map Screen1");
 			if( gTownLoyalty[ bTown ].fStarted && gfTownUsesLoyalty[ bTown ])
 			{
 				if ( g_lang == i18n::Lang::zh ) {
-					swprintf( sStringA, L"%d%��%% %s", gTownLoyalty[ bTown ].ubRating, gsLoyalString[ 0 ]);
+    				swprintf( sStringA, L"%d%£¥%% %s", gTownLoyalty[ bTown ].ubRating, gsLoyalString[ 0 ]);
 				} else {
 					swprintf( sStringA, L"%d%%%% %s", gTownLoyalty[ bTown ].ubRating, gsLoyalString[ 0 ]);
 				}
@@ -4875,7 +4875,7 @@ void BlitMineText( INT16 sMapX, INT16 sMapY )
 		if (GetMaxPeriodicRemovalFromMine(ubMineIndex) > 0)
 		{
 			if ( g_lang == i18n::Lang::zh ) {
-				swprintf( wSubString, L" (%d%��%%)", (PredictDailyIncomeFromAMine(ubMineIndex, TRUE) * 100 ) / GetMaxDailyRemovalFromMine(ubMineIndex) );
+    			swprintf( wSubString, L" (%d%£¥%%)", (PredictDailyIncomeFromAMine(ubMineIndex, TRUE) * 100 ) / GetMaxDailyRemovalFromMine(ubMineIndex) );
 			} else {
 				swprintf( wSubString, L" (%d%%%%)", (PredictDailyIncomeFromAMine(ubMineIndex, TRUE) * 100 ) / GetMaxDailyRemovalFromMine(ubMineIndex) );
 			}
